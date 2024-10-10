ef6918e2f33a20903bb4c21f2b5de60b49f29433
andreldgraciano <dedasgdias@gmail.com>
2024-10-10 14:55:15 -0300

    bg

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "index.html",
+      "./assets/**/*.js",
+    ],
+    theme: {
+      fontFamily:{
+        'sans': ['Roboto', 'sans-serif']
+      },
+      extend: {
+        backgroundImage:{
+          "home": "url('./assets/images/bg.jpg')",
+        },
+      },
+    },
+    plugins: [],
+  }
